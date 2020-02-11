cf5e11b2b4ed0754398987994b77ed53b205680c
Leon <yanglyu.leon.7@gmail.com>
2020-02-11 13:42:46 -0800

    tutorial: Adding a Room Database

---
+++ app/src/main/java/com/idiotleon/tutorialroom/db/WordRoomDatabase.kt
@@ -0,0 +1,38 @@
+package com.idiotleon.tutorialroom.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.idiotleon.tutorialroom.model.Word
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+@Database(entities = [Word::class], version = 1, exportSchema = false)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+        private const val NUMBER_OF_THREADS: Int = 4
+        val databseWriteExecutor: ExecutorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS)
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            INSTANCE?.let {
+                synchronized(it::class.java) {
+                    INSTANCE?.let {
+                        INSTANCE =
+                            Room.databaseBuilder(
+                                context.applicationContext,
+                                it::class.java,
+                                "word_database"
+                            )
+                                .build()
+                    }
+                }
+            }
+
+            return INSTANCE as WordRoomDatabase
+        }
+    }
+}
