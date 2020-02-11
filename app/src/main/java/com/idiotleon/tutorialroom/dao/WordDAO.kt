4978e6794493085a5957b1a4c96697a0be416f77
Leon <yanglyu.leon.7@gmail.com>
2020-02-11 11:18:02 -0800

    tutorial: creating the DAO

---
+++ app/src/main/java/com/idiotleon/tutorialroom/dao/WordDAO.kt
@@ -0,0 +1,19 @@
+package com.idiotleon.tutorialroom.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.idiotleon.tutorialroom.model.Word
+
+@Dao
+interface WordDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+}
